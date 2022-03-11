7a343a83f486e249e2e0ba760cdcac4045249d69
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2022-03-10 20:27:24 -0500

    Initial commit

---
+++ Formula/flasm.rb
@@ -0,0 +1,31 @@
+class Flasm < Formula
+  desc "Flash command-line assembler and disassembler"
+  homepage "https://www.nowrap.de/flasm.html"
+  url "http://www.nowrap.de/download/flasm16src.zip"
+  version "1.64"
+  sha256 "bb37695bccc8c03616b877779c6d659005f5ad0f2d6d0ab3937c38b2c0c6fa58"
+
+  def install
+    inreplace "Makefile", "keywords.gperf >", "--size-type=size_t keywords.gperf >"
+    inreplace "flasm.c",
+      "(iniFile = fopen(inipath, \"r\")) == NULL",
+      "(iniFile = fopen(inipath, \"r\")) == NULL && (iniFile = fopen(\"#{etc}/flasm.ini\", \"r\")) == NULL"
+    system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
+    bin.install "flasm"
+    etc.install "flasm.ini"
+  end
+
+  test do
+    (testpath/"test").write <<~EOS
+      constants 'a', 'b'
+      push 'a', 'b'
+      getVariable
+      push 'b'
+      getVariable
+      multiply
+      setVariable
+    EOS
+
+    system "#{bin}/flasm", "-b", "test"
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# homebrew-flasm
+# Homebrew flasm
+
 Hombrew tap for flasm
+
+[![Build Status](https://github.com/JrMasterModelBuilder/homebrew-flasm/workflows/main/badge.svg?branch=main)](https://github.com/JrMasterModelBuilder/homebrew-flasm/actions?query=workflow%3Amain+branch%3Amain)
+
+
+# Usage
+
+## Tap
+
+```
+$ brew tap JrMasterModelBuilder/flasm
+```
+
+## Install
+
+```
+$ brew install flasm
+```
